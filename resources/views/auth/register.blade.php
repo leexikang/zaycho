dcadc5b514d4a6f8f3b25dbf657be3652bb66b40
leexikang <leexikang@gmail.com>
2016-09-30 16:40:50 +0630

    redesign login and register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,80 +1,84 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                        {{ csrf_field() }}
+<br/>
+<br/>
+<div class="row">
+    <div class="small-8 small-centered columns">
+        <div class="row">
+            <div class="small-8 small-centered columns">
+                <h3 class="subheader"> Sign up</3>
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                <form class="form-horizontal" role="form" method="post" action="{{ url('/register') }}">
+                    {{ csrf_field() }}
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
+                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <label for="name" class="col-md-4 control-label">name</label>
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="col-md-6">
+                            <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required aria-describedby="exampleHelpText">
+
+                            @if ($errors->has('name'))
+                                 <small class="error"> {{ $errors->first('name') }} </small>
+                            @endif
                         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                    </div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label for="email" class="col-md-4 control-label">e-mail address</label>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                            @if ($errors->has('email'))
+                                <span class="error"> <small class="error"> {{ $errors->first('email') }} </small>  </span>
+                            @endif
+                            
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label for="password" class="col-md-4 control-label">password</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
+                        <div class="col-md-6">
+                            <input id="password" type="password" class="form-control" name="password">
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('password'))
+                                <span class="error"> <small class="error"> {{ $errors->first('password') }} </small> </span>
+                            @endif
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                    <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                        <label for="password-confirm" class="col-md-4 control-label">confirm password</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
+                        <div class="col-md-6">
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
 
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('password_confirmation'))
+                            <span class="error">
+                                <small>{{ $errors->first('password_confirmation') }} </small>
+                            </span>
+                            @endif
                         </div>
+                    </div>
+                    <div class="row">
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="small-12 columns">
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="expanded alert hollow button">
                                     <i class="fa fa-btn fa-user"></i> Register
-                                </button>
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </form>
-                </div>
+
+                    </div>
+
+                </form>
+
             </div>
         </div>
     </div>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,31 +1,36 @@
 
 <div id="navigation" class="top-bar" >
-    <div class="top-bar-title">
-        <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
-            <button class="menu-icon dark" type="button" data-toggle></button>
-        </span>
-        <strong>ZayCho</strong>
-    </div>
-    <div id="responsive-menu">
-        <div class="top-bar-left">
-            <ul class="dropdown menu" data-dropdown-menu>
-                <li>
-                    <a href="#">One</a>
-                    <ul class="menu vertical">
-                        <li><a href="#">One</a></li>
-                        <li><a href="#">Two</a></li>
-                        <li><a href="#">Three</a></li>
+
+ <div class="top-bar-left">
+
+    <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
+        <button class="menu-icon dark" type="button" data-toggle></button>
+    </span>
+
+    <ul class="menu">
+
+        <li class="menu-text"> Zaycho </li>
+        <li class="menuitem"><a href="#"> Hello </a></li>
+    </ul>
+
+</div>
+
+
+<div class="top-bar-right">
+        <div id="responsive-menu">
+            <div class="top-bar-left">
+                <ul class="dropdown menu" data-dropdown-menu>
+                    <li>
+                        <a href="#">{!! Auth::user()->name !!}</a>
+                        <ul class="menu vertical">
+                            <li><a href="#">One</a></li>
+                            <li><a href="#">Two</a></li>
+                            <li><a href="#">Logout</a></li>
+                        </ul>
+                    </li>
                     </ul>
-                </li>
-                <li><a href="#">Two</a></li>
-                <li><a href="#">Three</a></li>
-            </ul>
-        </div>
-        <div class="top-bar-right">
-            <ul class="menu">
-                <li><input type="search" placeholder="Search"></li>
-                <li><button type="button" class="button">Search</button></li>
-            </ul>
+                </div>
         </div>
     </div>
+
 </div>
